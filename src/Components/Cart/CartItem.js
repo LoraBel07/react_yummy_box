acf7d17db426c522ee9bfb025f5616926515a8fb
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-08-26 13:54:09 -0400

    commit

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -5,16 +5,12 @@ const CartItem = ({cartItem}) => {
 
 console.log(cartItem)
 
-// const dispatch=useDispatch();
-
 const dishes = dataDishes.find(item => item.id === cartItem.dishId);
 console.log(dishes);
 	return (<div>
-		{/* <p>{dishes.name}</p> */}
-		{/* <p>$ {dishes.price}</p> */}
+		<p>{dishes.name}</p>
+		<p>Price: ${dishes.price * cartItem.quantity}</p>
 		<p>{cartItem.quantity} porstion(s)</p>
-		<p>{cartItem.name}</p>
-		<p>$ {cartItem.price}</p>
 		
 	</div>)
 }
