759fe7f78b62812bc18c8964bf3971df5aee8657
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-08-27 16:18:05 -0400

    commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,14 @@
-import { getCartItems } from "../../redux/cartSlice";
+import { getCartItems, getTotalPrice } from "../../redux/cartSlice";
 import CartItem from "./CartItem";
 import { useSelector } from "react-redux";
 
 const Cart = () => {
 	const cartItems = useSelector(getCartItems);
+	const totalPrice = useSelector(getTotalPrice);
 
 	return (<div>
 		<img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png" alt=""/>
+		<h3>TOTAL: $ {totalPrice}</h3>
 		{cartItems.map(cartItem => <CartItem cartItem={cartItem}/>)}
 		
 	</div>)
--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -8,9 +8,10 @@ console.log(cartItem)
 const dishes = dataDishes.find(item => item.id === cartItem.dishId);
 console.log(dishes);
 	return (<div>
-		<p>{dishes.name}</p>
-		<p>Price: ${dishes.price * cartItem.quantity}</p>
+		<p>{dishes.name}</p>		
 		<p>{cartItem.quantity} porstion(s)</p>
+		<p>Price: ${dishes.price * cartItem.quantity}</p>
+		<img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt=""/> 
 		
 	</div>)
 }
